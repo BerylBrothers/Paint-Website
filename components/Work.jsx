9b1e96b7566fec67cbe4d46ecc0430908aa8049a
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-02-27 13:59:49 -0800

    Updated work section. In Progress

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link';
 import {
     FaHtml5,
     FaCss3, FaJs, FaReact, FaFigma, FaNodeJs
@@ -14,6 +15,7 @@ const projects = [
   {
     title: 'Aqua Solutions',
     description: "The Aqua Solutions website is designed with a clean, modern, and user-friendly interface that reflects the professionalism and reliability of the plumbing company. The website offers a seamless experience for users who are looking for high-quality plumbing services, whether for residential or commercial needs",
+    href:"/",
     stack:[
       {
         name:< FaFigma />
@@ -30,6 +32,7 @@ const projects = [
   {
     title: 'The Lotus Garden',
     description: "The Lotus Garden website features a clean, elegant design that reflects the restaurant's authentic Asian flavors and inviting atmosphere. The interactive menu showcases a variety of dishes with high-quality images and detailed descriptions, while the About Us section shares the restaurant's story and dedication to quality.",
+    href:"/",
     stack:[
       {
         name:< FaFigma />
@@ -46,6 +49,7 @@ const projects = [
   {
     title: 'Pulse Fitness',
     description: "I created a sleek and user-friendly website for Pulse Fitness that showcases the gym's offerings in a modern and engaging way. The design emphasizes simplicity and ease of navigation, highlighting key features like membership options, class schedules, and contact information blah blah blah blah.",
+    href:"/",
     stack:[
       {
         name:< SiNextdotjs />
@@ -62,37 +66,40 @@ const projects = [
 ]
 
 const Work = () => {
-  return (
-    <section className='h-full bg-white text-black'>
-    <div className="container mx-auto h-full">
-        <div className='py-12'>
-            <h3 className='py-8'>My Work</h3>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-            {projects.map((project, index) => {
-                return <div className='border border-black' key={index}>
-                    <div className='border border-black'>
-                    <Image 
-                    src={project.image} 
-                    alt={project.title} 
-                    width={500}  // You can set a fixed width
-                    height={350} // You can set a fixed height
-                    objectFit="cover" // Ensures the image covers the div without distortion
-                  />
+    return (
+        <section className='h-full bg-white text-black'>
+          <div className="container mx-auto h-full">
+            <div className='py-12'>
+              <h3 className='py-12'>My Work</h3>
+              <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                {projects.map((project, index) => {
+                  return (
+                    <div className='border border-black flex flex-col' key={index}>
+                      <Link href={project.href}>
+                          <div className='relative h-80'>
+                            <Image
+                              src={project.image}
+                              alt={project.title}
+                              layout="fill"  // This makes the image take up the full container
+                              objectFit="cover" // Ensures the image covers the div without distortion
+                            />
+                          </div>
+                      </Link>
+                      <div className="p-2 pt-4 flex flex-row">
+                        {/* Map over the stack array to render each icon */}
+                        {project.stack.map((tech, idx) => (
+                          <span key={idx} className="mr-2 text-xl">{tech.name}</span>
+                        ))}
+                      </div>
+                      <div className='p-2'>{project.description}</div>
                     </div>
-                    <div className="p-2 pt-4 flex flex-row">
-                    {/* Map over the stack array to render each icon */}
-                    {project.stack.map((tech, idx) => (
-                      <span key={idx} className="mr-2 text-xl">{tech.name}</span>
-                    ))}
-                  </div>
-                    <div className='p-2'>{project.description}</div>
-                    </div>
-            })}
-        </div>
-        </div>
-    </div>
-    </section>
-  )
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </section>
+      )
 }
 
 export default Work
