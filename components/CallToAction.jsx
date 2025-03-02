620fb607c7629078c0702c6ee343f93be35be6f8
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-03-02 13:39:42 -0800

    testing-3

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const CallToAction = () => {
     return (
-        <section className='min-h-screen bg-white'>
+        <section className='h-full bg-white'>
             <div className="container mx-auto flex justify-center items-center">
                 <div className='flex justify-center items-center py-24'>
-                    <div className='flex justify-center items-center h-auto w-[90%] sm:w-[700px] rounded-xl border bg-primary border-white'>
+                    {/* Set the width for mobile (w-full), medium screens (w-[90%]), and large screens (w-[700px]) */}
+                    <div className='flex justify-center items-center w-[500px] h-[300px] px-6 rounded-xl border bg-primary border-white'>
                         Call to Action
                     </div>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default CallToAction;
