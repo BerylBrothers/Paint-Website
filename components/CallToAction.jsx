3d219b16b12db1997d253ab146e8e2d320e229ba
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-03-02 13:42:50 -0800

    testing-4

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CallToAction = () => {
     return (
-        <section className='h-full bg-white'>
+        <section className='h-full bg-white overflow-hidden'>
             <div className="container mx-auto flex justify-center items-center">
                 <div className='flex justify-center items-center py-24'>
                     {/* Set the width for mobile (w-full), medium screens (w-[90%]), and large screens (w-[700px]) */}
