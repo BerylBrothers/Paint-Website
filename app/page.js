ae0c8b946a5b56c9c39470123dff0c6b84b82857
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-03-02 13:26:10 -0800

    Testing

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
     <Work />
     <About />
     <Services />
-    {/* <CallToAction /> */}
+    <CallToAction />
     </>
   );
   
--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -5,7 +5,7 @@ const CallToAction = () => {
         <section className='h-full bg-white'>
      <div className="container mx-auto flex justify-center items-center ">
 
-        <div className='h-[500px] w-[700px] flex justify-center items-center  border border-white'>
+        <div className='flex justify-center items-center py-24'>
            <div className='flex justify-center items-center h-[300px] w-[700px] rounded-xl border bg-primary border-white'>Call to Action</div>
             </div>
         
