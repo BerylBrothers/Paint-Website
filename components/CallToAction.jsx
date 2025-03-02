e2fb7d9b94901b609bb5f1ee7e70b8f95714a1e5
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-03-02 13:30:18 -0800

    testing-2

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 
 const CallToAction = () => {
     return (
-        <section className='h-full bg-white'>
-     <div className="container mx-auto flex justify-center items-center ">
-
-        <div className='flex justify-center items-center py-24'>
-           <div className='flex justify-center items-center h-[300px] w-[700px] rounded-xl border bg-primary border-white'>Call to Action</div>
+        <section className='min-h-screen bg-white'>
+            <div className="container mx-auto flex justify-center items-center">
+                <div className='flex justify-center items-center py-24'>
+                    <div className='flex justify-center items-center h-auto w-[90%] sm:w-[700px] rounded-xl border bg-primary border-white'>
+                        Call to Action
+                    </div>
+                </div>
             </div>
-        
-        
-        </div>
         </section>
-       )
+    )
 }
 
-export default CallToAction
+export default CallToAction;
