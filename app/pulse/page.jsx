e1eb05a7ee934753c4b8a14680cbd4deb1524ac0
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-03-07 12:55:03 -0800

    scroll animation test 9

--- app/pulse/page.jsx
+++ app/pulse/page.jsx
@@ -66,7 +66,7 @@ const Pulse = () => {
                                     alt="Lotus Garden Showcase"
                                     height={1200}
                                     width={1200}
-                                    className="w-full h-auto object-cover transition-transform duration-1000 ease-in-out group-hover:translate-y-[-62%] xl:group-hover:translate-y-[-76.5%]"
+                                    className="w-full h-auto object-cover transition-transform duration-1000 ease-in-out group-hover:translate-y-[-62.5%] xl:group-hover:translate-y-[-76.5%]"
                                 />
                             </div>
                         </div>
