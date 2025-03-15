c2592bcff924bd7cbb8aec11dbe7fbed425ffc5b
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-03-15 13:39:22 -0700

    added hover animations to hero page

--- components/Services.jsx
+++ components/Services.jsx
@@ -43,33 +43,37 @@ const services = [
 const Services = () => {
 
   return (
-    <section className="h-full text-black bg-white">
-      <div className="container mx-auto h-full">
-        <div className="py-10">
-          <h2 className="text-center pt-6 pb-20">Crafting Beautiful, Functional Websites</h2>
-          <div className="flex pt-12 justify-center items-center">
-            <div className="grid md:grid-cols-3 grid-rows-2 grid-cols-1 gap-20"> {/* Increased gap */}
-              {services.map((service, index) => (
-                <Link href={"/services"} key={index} className="relative">
-                  {/* Icon Container */}
-                  <div className="absolute flex justify-center items-center border h-[110px] w-[110px] border-black p-2 -top-16 left-1/2 transform -translate-x-1/2 bg-primary text-white rounded-xl -mb-8 mx-auto z-10"> {/* Added margin bottom */}
-                    <div className="flex items-center justify-center text-4xl">{service.icons}</div>
-                  </div>
-  
-                  {/* Description div */}
-                  <div className="flex justify-center items-center border text-[14px] h-[350px] w-[350px] bg-gray-600/10 text-black p-12 rounded-xl transition-shadow duration-300 hover:shadow-lg">
-                    <div className="flex flex-col justify-center items-center">
-                      <h3 className="m-4 pt-2 text-center">{service.title}</h3>
-                      <div>{service.description}</div>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </div>
+<section className="h-full text-black bg-white">
+  <div className="container mx-auto h-full">
+    <div className="py-10">
+      <h2 className="text-center pt-6 pb-20">Crafting Beautiful, Functional Websites</h2>
+      <div className="flex pt-12 justify-center items-center">
+        <div className="grid md:grid-cols-3 grid-rows-2 grid-cols-1 gap-20">
+          {services.map((service, index) => (
+            <Link href="/services" key={index} className="relative group"> {/* Added group class here */}
+              
+              {/* Icon Container */}
+              <div className="absolute flex justify-center items-center border h-[110px] w-[110px] border-black p-2 -top-16 left-1/2 transform -translate-x-1/2 bg-primary text-white rounded-xl transition-all duration-300 -mb-8 mx-auto z-10 group-hover:bg-blue-500 group-hover:text-white"> {/* group-hover to trigger on hover */}
+                <div className="flex items-center justify-center text-4xl">
+                  {service.icons}
+                </div>
+              </div>
+
+              {/* Description div */}
+              <div className="flex justify-center items-center border text-[14px] h-[350px] w-[350px] bg-gray-600/10 text-black p-12 rounded-xl transition-all duration-300 ease-in-out hover:bg-primary hover:text-white hover:scale-105 group-hover:bg-primary group-hover:text-white"> {/* Added group-hover here */}
+                <div className="flex flex-col justify-center items-center">
+                  <h3 className="m-4 pt-2 text-center">{service.title}</h3>
+                  <div>{service.description}</div>
+                </div>
+              </div>
+
+            </Link>
+          ))}
         </div>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
   );
 }
 
