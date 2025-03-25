aaedbf004eb750cd694fa25985e84e35c6dbc281
Live session user <Daniel@Berylbrothers.com>
2025-03-25 01:46:02 -0400

    parallax test20

--- app/page.js
+++ app/page.js
@@ -1,22 +1,22 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
-import ParallaxComponent from "@/components/ParallaxComponent";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Team from "@/components/Team";
 import About2 from "@/components/About2";
+import ParallaxComponent from "@/components/ParallaxComponent";
 
 export default function Home() {
   return (
-    <div className="overflow:hidden">
+  
     <>
 <Hero />
-<ParallaxComponent />
+<ParallaxComponent/>
 <About />
 <About2 />
 <Services />
 <Team />
 </>
-</div>
+
   );
 }
--- components/About.jsx
+++ components/About.jsx
@@ -1,48 +1,49 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const About = () => {
-    return (
-        <section>
-            <div className="container mx-auto pt-12 pb-6">
-                <div className="flex xl:flex-row flex-col justify-between items-center gap-4">
-                    {/* Image Section */}
-                    <div className="flex flex-col xl:flex-row py-12 ">
-                        <Image 
-                            src={'/images/tree-image-3.jpg'}
-                            height={600}
-                            width={600}
-                            alt="Tree Image"
-                            className="max-h-[700px] w-auto" 
-                        />
-                    </div>
-    
-                    {/* Text Section */}
-                    <div className="flex flex-col justify-center items-center text-left p-6">
-                        <div className='text-center xl:text-left '> 
-                            <h5 className='p-6'>Professional Land & Tree Maintenance</h5>
-                            <h2 className=" p-6 max-w-[400px]">Tree Removal Is An Easy Task</h2>
-                        <p className='max-w-[600px] p-6'>Higuera's tree service is proud to serve the comunity work at competitive prices.
-                            <br/>
-                            <br/>
-                            Since the start of our company we have strived to obtain the best quality tools so that the jobs are done to the highest standards.
-                            </p>
-                            <div className='p-6'>
-                            <a href="#" className="button">Hover me</a>
-                            </div>
-                            </div>
-                          
-  
-                       
-                    </div>
-                </div>
-          
-  
- 
+  return (
+    <section className="bg-white z-10 text-black">
+      <div className="container mx-auto pt-12 pb-6">
+        <div className="flex xl:flex-row flex-col justify-between items-center gap-4">
+          {/* Image Section */}
+          <div className="flex flex-col xl:flex-row py-12 ">
+            <Image
+              src={"/images/tree-image-3.jpg"}
+              height={600}
+              width={600}
+              alt="Tree Image"
+              className="max-h-[700px] w-auto"
+            />
+          </div>
 
+          {/* Text Section */}
+          <div className="flex flex-col justify-center items-center text-left p-6 ">
+            <div className="text-center xl:text-left ">
+              <h5 className="p-6">Professional Land & Tree Maintenance</h5>
+              <h2 className=" p-6 max-w-[400px]">
+                Tree Removal Is An Easy Task
+              </h2>
+              <p className="max-w-[600px] p-6">
+                Higuera's tree service is proud to serve the comunity work at
+                competitive prices.
+                <br />
+                <br />
+                Since the start of our company we have strived to obtain the
+                best quality tools so that the jobs are done to the highest
+                standards.
+              </p>
+              <div className="p-6">
+                <a href="#" className="button z-10">
+                  Hover me
+                </a>
+              </div>
             </div>
-        </section>
-      );
-}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
