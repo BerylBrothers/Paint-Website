1e421141040827d9bfc32936f4d3cca0dfc3a73a
Live session user <Daniel@Berylbrothers.com>
2025-03-24 22:24:12 -0400

    parallax test13

--- components/ParallaxComponent.jsx
+++ components/ParallaxComponent.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import Image from "next/image";
 
 // Items
@@ -23,54 +22,43 @@ const items = [
 ];
 
 const ParallaxComponent = () => {
-  // Adjust background style for mobile and desktop
-  const backgroundStyle = {
-    backgroundImage: 'url("/images/logs.jpg")',
-    backgroundSize: "cover",
-    backgroundPosition: "center center", // Keep the fixed background for desktop
-    backgroundAttachment: "fixed", // Use scroll on mobile and fixed on desktop
-    WebkitBackfaceVisibility: "hidden", // Add for Safari and Chrome (Webkit browsers)
-    backfaceVisibility: "hidden", // Standard CSS property for other browsers
-  };
-
   return (
-    <div
-      className="relative w-full h-[70vh] xl:h-[50vh] bg-cover bg-bottom bg-no-repeat flex items-center py-12 justify-center"
-      style={backgroundStyle}
-    >
-      <div className="absolute inset-0 bg-black opacity-80"></div>
-      <section>
-        <div className="container mx-auto text-center px-4">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {items.map((item, index) => (
-              <div
-                className="flex flex-col justify-center items-center text-white z-10 group"
-                key={index}
-              >
-                <div className="mb-4 opacity-50 transition-all duration-300 hover:opacity-100">
-                  <div className="flex items-center justify-center gap-4 w-full">
-                    <div className="relative w-10 h-10 xl:w-20 xl:h-20">
-                      <Image
-                        src={item.image}
-                        alt=""
-                        height={100}
-                        width={100}
-                        className="rounded-full object-cover"
-                      />
+    <div>
+      <div className="relative-container">
+        <div className="overlay"></div>
+        <section>
+          <div className="container mx-auto text-center px-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {items.map((item, index) => (
+                <div
+                  className="flex flex-col justify-center items-center text-white z-10 group"
+                  key={index}
+                >
+                  <div className="mb-4 opacity-50 transition-all duration-300 hover:opacity-100">
+                    <div className="flex items-center justify-center gap-4 w-full">
+                      <div className="relative w-10 h-10 xl:w-20 xl:h-20">
+                        <Image
+                          src={item.image}
+                          alt=""
+                          height={100}
+                          width={100}
+                          className="rounded-full object-cover"
+                        />
+                      </div>
+                      <div className="flex flex-col justify-center">
+                        <h3>{item.action}</h3>
+                      </div>
                     </div>
-                    <div className="flex flex-col justify-center">
-                      <h3>{item.action}</h3>
+                    <div className="border-t-2 border-white py-4 mt-4 sm:border-t-1 sm:py-2 md:border-t-2 md:py-4 transition-all duration-300 group-hover:border-green-800">
+                      <p>{item.description}</p>
                     </div>
-                  </div>
-                  <div className="border-t-2 border-white py-4 mt-4 sm:border-t-1 sm:py-2 md:border-t-2 md:py-4 transition-all duration-300 group-hover:border-green-800">
-                    <p>{item.description}</p>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
