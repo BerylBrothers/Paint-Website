89b0f97eb3ca32d42f59b8b292ecfdc0b0dc2781
Live session user <Daniel@Berylbrothers.com>
2025-03-25 00:45:29 -0400

    made change to parallax component

--- components/ParallaxComponent.jsx
+++ components/ParallaxComponent.jsx
@@ -23,37 +23,39 @@ const items = [
 
 const ParallaxComponent = () => {
   return (
-    <section className="parallax-background relative w-full h-[70vh] xl:h-[50vh] bg-cover bg-bottom bg-no-repeat flex items-center py-12 justify-center">
-      <div className="overlay"></div>
+    <section className="relative w-full h-[70vh] xl:h-[50vh] flex items-center justify-center">
+      <div className="parallax-background">
+        <div className="overlay"></div>
 
-      <div className="container mx-auto text-center px-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {items.map((item, index) => (
-            <div
-              className="flex flex-col justify-center items-center text-white z-10 group"
-              key={index}
-            >
-              <div className="mb-4 opacity-50 transition-all duration-300 hover:opacity-100">
-                <div className="flex items-center justify-center gap-4 w-full">
-                  <div className="relative w-10 h-10 xl:w-20 xl:h-20">
-                    <Image
-                      src={item.image}
-                      alt=""
-                      height={100}
-                      width={100}
-                      className="rounded-full object-cover"
-                    />
+        <div className="container mx-auto text-center px-4 flex justify-center items-center h-full">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {items.map((item, index) => (
+              <div
+                className="flex flex-col justify-center items-center text-white z-10 group"
+                key={index}
+              >
+                <div className="mb-4 opacity-50 transition-all duration-300 hover:opacity-100">
+                  <div className="flex items-center justify-center gap-4 w-full">
+                    <div className="relative w-10 h-10 xl:w-20 xl:h-20">
+                      <Image
+                        src={item.image}
+                        alt=""
+                        height={100}
+                        width={100}
+                        className="rounded-full object-cover"
+                      />
+                    </div>
+                    <div className="flex flex-col justify-center">
+                      <h3>{item.action}</h3>
+                    </div>
                   </div>
-                  <div className="flex flex-col justify-center">
-                    <h3>{item.action}</h3>
+                  <div className="border-t-2 border-white py-4 mt-4 sm:border-t-1 sm:py-2 md:border-t-2 md:py-4 transition-all duration-300 group-hover:border-green-800">
+                    <p>{item.description}</p>
                   </div>
-                </div>
-                <div className="border-t-2 border-white py-4 mt-4 sm:border-t-1 sm:py-2 md:border-t-2 md:py-4 transition-all duration-300 group-hover:border-green-800">
-                  <p>{item.description}</p>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </section>
