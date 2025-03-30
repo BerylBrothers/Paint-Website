86a7655fa38893c6d1d22d7ef33e00398e373bbc
Live session user <Daniel@Berylbrothers.com>
2025-03-30 15:04:07 -0400

    started contact section

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 import PageTitle from "@/components/PageTitle";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import { Textarea } from "../../components/ui/textarea";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const cards = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    info: "9570-039-03904",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    info: "123 oak st.",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    info: "Service@TreeCutting.com",
+  },
+];
 
 const page = () => {
   return (
@@ -7,7 +30,24 @@ const page = () => {
       <div>
         <PageTitle title="Contact" />
       </div>
-      <div className="container">Contact</div>
+      <div className="flex justify-center items-center mx-auto container">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-12">
+          {cards.map((card, index) => {
+            return (
+              <div
+                className="flex justify-center items-center border border-black h-[350px] w-[350px]"
+                key={index}
+              >
+                <div className="flex flex-col justify-center items-center">
+                  <h3 className="py-2">{card.icon}</h3>
+                  <h3 className="py-2">{card.title}</h3>
+                  <p>{card.info}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
