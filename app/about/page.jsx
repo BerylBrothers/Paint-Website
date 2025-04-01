a5e16e135de1915b1a625e80dcd2a6424218e2c7
Live session user <Daniel@Berylbrothers.com>
2025-04-01 11:49:31 -0400

    added About Us Page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 import PageTitle from "@/components/PageTitle";
+import Image from "next/image";
+
+const items = [
+  {
+    picture: "/images/lumberjack-1-resize.jpg",
+    name: "Eric Lopez",
+    title: "Tree Service",
+  },
+  {
+    picture: "/images/lumberjack-2-resize.jpg",
+    name: "John Phillips",
+    title: "Tree Service",
+  },
+  {
+    picture: "/images/lumberjack-3-resize.jpg",
+    name: "Lance Baker",
+    title: "Tree Service",
+  },
+];
 
 const page = () => {
   return (
@@ -7,7 +26,118 @@ const page = () => {
       <div>
         <PageTitle title="About Us" />
       </div>
-      <div className="container">About</div>
+      <div className="container mx-auto pt-6 pb-12">
+        <div className="flex xl:flex-row flex-col justify-between items-center gap-4">
+          {/* Text Section */}
+          <div className="flex flex-col xl:order-1 order-2 justify-center items-center text-left p-6">
+            <div className="text-center xl:text-left">
+              <div className="flex flex-col xl:justify-start justify-center xl:items-start items-center p-6">
+                <p className="py-2">Professional Land & Tree Maintenance</p>
+                <div className=" flex justify-center items-center border-green-800 border-b-2 w-16 "></div>
+              </div>
+              <h2 className=" p-6 max-w-full xl:max-w-[400px]">
+                Our Mission Statement
+              </h2>
+              <p className="max-w-[600px] p-6">
+                Higuera's tree service is proud to serve the comunity work at
+                competitive prices.
+                <br />
+                <br />
+                Since the start of our company we have strived to obtain the
+                best quality tools so that the jobs are done to the highest
+                standards.
+              </p>
+              <div className="p-6">
+                <div id="container">
+                  <button className="learn-more button-style">
+                    <span className="circle" aria-hidden="true">
+                      <span className="icon arrow"></span>
+                    </span>
+                    <span className="button-text">Learn More</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Image Section */}
+
+          <div className="flex flex-col xl:flex-row py-12 xl:order-1 order-1">
+            <Image
+              src={"/images/tree-image-4-resize.jpg"}
+              height={800}
+              width={600}
+              alt="Tree Image"
+              className="max-h-[700px] w-auto"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="relative flex flex-col justify-center items-center w-full forest-bg-2 h-[45vh]">
+        <div className="absolute inset-0 opacity-60 bg-black "></div>
+        {/* Display only the current testimonial */}
+        <div className="relative z-10 ">
+          <div className="flex flex-col justify-center items-center text-center xl:text-left p-6 z-10 text-white ">
+            <h2 className="flex justify-center items p-6 max-w-full xl:max-w-[350px] text-center">
+              Join Our Tree Service
+            </h2>
+            <p className="max-w-[600px]">
+              Higuera's tree service is proud to serve the community with
+              competitive prices.
+              <br />
+              <br />
+              Since the start of our company, we have strived to obtain the best
+              quality tools so that the jobs are done to the highest standards.
+            </p>
+            <div className="">
+              <a href="#" className="button z-10">
+                Hover me
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col xl:flex justify-center items-center py-12">
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center items-center p-6">
+            <p className="py-2">Professional Land & Tree Maintenance</p>
+            <div className="flex justify-center items-center border-green-800 border-b-2 w-16 "></div>
+          </div>
+          <div className="py-12">
+            <h2>Our Lumberjacks</h2>
+          </div>
+          <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+            {items.map((item, index) => {
+              return (
+                <a href="/" key={index} className="group">
+                  <div className="relative">
+                    {/* Wrapper div around the image */}
+                    <div className="relative overflow-hidden">
+                      <Image
+                        src={item.picture}
+                        height={400}
+                        width={400}
+                        alt=""
+                        className="transition-all duration-500 ease-in-out"
+                      />
+                      {/* Black overlay directly on the image */}
+                      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-500"></div>
+                    </div>
+
+                    {/* Image text content */}
+                    <div className="py-2">
+                      <h3>{item.name}</h3>
+                    </div>
+                    <div className="border-green-800 border-b-2 w-16 "></div>
+                    <div>
+                      <p className="py-2">{item.title}</p>
+                    </div>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
